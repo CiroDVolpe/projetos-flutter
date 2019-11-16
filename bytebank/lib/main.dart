01b1fe018416216f8be30404e2e9af5c3620b430
Ciro <cirodvolpe@gmail.com>
2019-11-16 20:28:59 -0300

    Feitos alguns exercicios do curso da Alura

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -1,30 +1,76 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('Transferências'),
-    ),
-    body: Column(
-      children: <Widget>[
-        Card(
-          child: ListTile(
-            leading: Icon(Icons.monetization_on),
-            title: Text('100.0'),
-            subtitle: Text('1000'),
-          ),
-        ),
-        Card(
-          child: ListTile(
-            leading: Icon(Icons.monetization_on),
-            title: Text('200.0'),
-            subtitle: Text('2000'),
-          ),
-        ),
-      ],
-    ),
-    floatingActionButton: FloatingActionButton(
-      child: Icon(Icons.add),
-    ),
-  ),
-));
+void main() => runApp(BytebankApp());
+
+class BytebankApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        body: TransferenceForm(),
+      ),
+    );
+  }
+}
+
+class TransferenceForm extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('New Transference'),
+      ),
+      body: Text('teste'),
+    );
+  }
+}
+
+class TransferenceList extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Transferences'),
+      ),
+      body: Column(
+        children: <Widget>[
+          TransferenceItem(Transference(100.0, 1001)),
+          TransferenceItem(Transference(200.0, 1010)),
+          TransferenceItem(Transference(300.0, 1100)),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class TransferenceItem extends StatelessWidget {
+
+  final Transference _transference;
+
+  TransferenceItem(this._transference);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(this._transference.value.toString()),
+        subtitle: Text(this._transference.accountNumber.toString()),
+      ),
+    );
+  }
+}
+
+class Transference {
+  final double value;
+  final int accountNumber;
+
+  Transference(this.value, this.accountNumber);
+}
+
