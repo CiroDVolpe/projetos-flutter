86419c6181caacaf26bda43233f5e3b1381f5c9d
Ciro <cirodvolpe@gmail.com>
2019-11-16 02:31:00 -0300

    Realizado exercicio 02 Aula 02 do curso de flutter da Alura

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -5,6 +5,24 @@ void main() => runApp(MaterialApp(
     appBar: AppBar(
       title: Text('Transferências'),
     ),
+    body: Column(
+      children: <Widget>[
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.monetization_on),
+            title: Text('100.0'),
+            subtitle: Text('1000'),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.monetization_on),
+            title: Text('200.0'),
+            subtitle: Text('2000'),
+          ),
+        ),
+      ],
+    ),
     floatingActionButton: FloatingActionButton(
       child: Icon(Icons.add),
     ),
