3297cb1dd4609898efcff6b83ffdd960a7a4521d
Ciro <cirodvolpe@gmail.com>
2020-05-23 12:45:54 -0300

    Iniciada modificacao para atualizacao dinamica da lista de contatos

--- bytebank2/lib/screens/contacts_list.dart
+++ bytebank2/lib/screens/contacts_list.dart
@@ -1,34 +1,26 @@
+import 'package:bytebank2/models/contact.dart';
 import 'package:bytebank2/screens/contact_form.dart';
 import 'package:flutter/material.dart';
 
 class ContactsList extends StatelessWidget {
+
+  final List<Contact> contacts = List();
+
   @override
   Widget build(BuildContext context) {
+    contacts.add(Contact(0, 'Ciro Della Volpe', 6913));
     return Scaffold(
       appBar: AppBar(
         title: Text(
           'Contacts',
         ),
       ),
-      body: ListView(
-        children: <Widget>[
-          Card(
-            child: ListTile(
-              title: Text(
-                'Meu Nome',
-                style: TextStyle(
-                  fontSize: 24.0,
-                ),
-              ),
-              subtitle: Text(
-                'Meu Sobrenome',
-                style: TextStyle(
-                  fontSize: 16.0,
-                ),
-              ),
-            ),
-          )
-        ],
+      body: ListView.builder(
+        itemBuilder: (context, index){
+          final Contact contact = contacts[index];
+          return _ContactItem(contact);
+        },
+        itemCount: contacts.length,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -44,6 +36,33 @@ class ContactsList extends StatelessWidget {
         },
         child: Icon(
           Icons.add,
+        ),
+      ),
+    );
+  }
+}
+
+class _ContactItem extends StatelessWidget{
+
+  final Contact contact;
+
+  _ContactItem(this.contact);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(
+          contact.name,
+          style: TextStyle(
+            fontSize: 24.0,
+          ),
+        ),
+        subtitle: Text(
+          contact.accountNumber.toString(),
+          style: TextStyle(
+            fontSize: 16.0,
+          ),
         ),
       ),
     );
