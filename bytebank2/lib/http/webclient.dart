808cc22e526e2d918103885384069adb659f1dbd
Ciro <cirodvolpe@gmail.com>
2020-05-24 22:25:31 -0300

    Implementados mais cenarios de erros

--- bytebank2/lib/http/webclient.dart
+++ bytebank2/lib/http/webclient.dart
@@ -2,7 +2,9 @@ import 'package:http/http.dart';
 import 'package:http_interceptor/http_interceptor.dart';
 import 'interceptors/logging_interceptor.dart';
 
-final Client client =
-    HttpClientWithInterceptor.build(interceptors: [LoggingInterceptor()]);
+final Client client = HttpClientWithInterceptor.build(
+    interceptors: [LoggingInterceptor()],
+    requestTimeout: Duration(seconds: 5),
+);
 
 const baseUrl = 'http://192.168.0.31:8080/transactions';
