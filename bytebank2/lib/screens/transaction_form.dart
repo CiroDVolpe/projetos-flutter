6a030e6b4d70ae965615918418bf8f624a2629a0
Ciro <cirodvolpe@gmail.com>
2020-05-25 01:59:37 -0300

    Finalizado segundo curso de flutter com web api. Implementado load enquanto espera carregar dados.

--- bytebank2/lib/screens/transaction_form.dart
+++ bytebank2/lib/screens/transaction_form.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:bytebank2/components/progress.dart';
 import 'package:bytebank2/components/response_dialog.dart';
 import 'package:bytebank2/components/transaction_auth_dialog.dart';
 import 'package:bytebank2/http/webclients/transaction_webclient.dart';
@@ -22,6 +23,8 @@ class _TransactionFormState extends State<TransactionForm> {
   final TransactionWebClient _webClient = TransactionWebClient();
   final String transactionId = Uuid().v4();
 
+  bool _sending = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,6 +37,13 @@ class _TransactionFormState extends State<TransactionForm> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
+              Visibility(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Progress(message: 'Sending',),
+                ),
+                visible: _sending,
+              ),
               Text(
                 widget.contact.name,
                 style: TextStyle(
@@ -98,6 +108,7 @@ class _TransactionFormState extends State<TransactionForm> {
     String password,
     BuildContext context,
   ) async {
+
     Transaction transaction = await _send(
       transactionCreated,
       password,
@@ -120,6 +131,9 @@ class _TransactionFormState extends State<TransactionForm> {
 
   Future<Transaction> _send(Transaction transactionCreated, String password,
       BuildContext context) async {
+    setState(() {
+      _sending = true;
+    });
     final Transaction transaction =
         await _webClient.save(transactionCreated, password).catchError((e) {
       _showFailureMessage(context, message: e.message);
@@ -128,7 +142,11 @@ class _TransactionFormState extends State<TransactionForm> {
           message: 'timeout submitting the transaction');
     }, test: (e) => e is TimeoutException).catchError((e) {
       _showFailureMessage(context);
-    });
+    }).whenComplete(() {
+      setState(() {
+        _sending = false;
+      });
+        });
     return transaction;
   }
 
